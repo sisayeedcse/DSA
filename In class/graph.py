f96002fb06186951baa4169489d9a03401d41d73
sisayeedcse <118920944+sisayeedcse@users.noreply.github.com>
2023-09-10 14:02:35 +0600

    Update graph.py

--- In class/graph.py
+++ In class/graph.py
@@ -7,6 +7,21 @@ class Graph:
     def add_edge(self, S,D):
         self.G[S][D]=1
         self.G[D][S]=1
+    def BFS(self,st):
+        Vs = [False]*self.V
+        Q = []
+        Q.append(st)
+        Vs[st] = True
+        while Q:
+            F = Q.pop(0)
+            print(F,end=" ")
+            #Q.pop(0)
+            for i in range(self.V):
+                if self.G[F][i] == 1 and not Vs[i]:
+                    Q.append(i)
+                    Vs[i] = True
+                    
+                    
     def print_G(self):
         for i in range(self.V):
           for j in range(self.V):
@@ -20,4 +35,6 @@ g.add_edge(1,2)
 g.add_edge(2,3)
 g.add_edge(3,4)
 g.add_edge(4,5)
+
 g.print_G()
+g.BFS(0)
